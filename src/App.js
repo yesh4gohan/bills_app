6d679b83d4f960229934434ce2a726cb84a795c8
Yashwanth <yashwanth@admins-MacBook-Pro.local>
2020-02-09 12:46:02 +0530

    basic camera feature working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import React from 'react';
-import './App.scss';
-import CapturePhoto from './capturePhoto';
+import React, { Fragment } from 'react';
+import ReactDOM from 'react-dom';
+import { CameraFeed } from './capturePhoto';
 
-function App() {
-  return (
-    <CapturePhoto/>  
-  );
+import './styles.css';
+import fs from 'fs';
+
+// Upload to local seaweedFS instance
+const uploadImage = async file => {
+    const formData = new FormData();
+    const date = new Date();
+    fs.writeFile(`/image_files/${date.now}`,file)
+    formData.append('file', file);
+    
+    // Connect to a seaweedfs instance
+};
+
+export default function App() {
+    return (
+        <div className="App">
+            <h1>Image capture test</h1>
+            <p>Capture image from USB webcamera and upload to form</p>
+            <CameraFeed sendFile={uploadImage} />
+        </div>
+    );
 }
 
-export default App;
+const rootElement = document.getElementById('root');
+ReactDOM.render(<App />, rootElement);
--- src/capturePhoto.jsx
+++ src/capturePhoto.jsx
@@ -1,31 +1,74 @@
-import React, { Component } from 'react'
-import {initializeMedia} from './utlity'
-const styles = {
-    width :Math.max(document.documentElement.clientWidth, window.innerWidth || 0) -50
-};
-export default class CapturePhoto extends Component {
-    async componentDidMount(){
-        const media = await initializeMedia();
-        this.showVideo(media)
-    }
-    showVideo(media){
-        const VideoPlayer = this.myRef.current;
-        console.log(VideoPlayer)
-        VideoPlayer.srcObject = media;
-        VideoPlayer.style.display = 'block';
+import React, { Component } from 'react';
+
+export class CameraFeed extends Component {
+    /**
+     * Processes available devices and identifies one by the label
+     * @memberof CameraFeed
+     * @instance
+     */
+    // processDevices(devices) {
+    //     devices.forEach(device => {
+    //         console.log(device.label);
+    //         this.setDevice(device);
+    //     });
+    // }
+
+    /**
+     * Sets the active device and starts playing the feed
+     * @memberof CameraFeed
+     * @instance
+     */
+    async setDevice() {
+        const stream = await navigator.mediaDevices.getUserMedia({ audio: false, video: true });
+        console.log(
+            stream
+        )
+        this.videoPlayer.srcObject = stream;
+        this.setState({stream})
     }
     state = {
-        data:[]
+        stream:{}
     }
-    myRef = React.createRef();
+    /**
+     * On mount, grab the users connected devices and process them
+     * @memberof CameraFeed
+     * @instance
+     * @override
+     */
+    async componentDidMount() {
+        await this.setDevice();
+    }
 
-    
+    /**
+     * Handles taking a still image from the video feed on the camera
+     * @memberof CameraFeed
+     * @instance
+     */
+    takePhoto = () => {
+        console.log(this.state)
+        const { sendFile } = this.props;
+        const context = this.canvas.getContext('2d');
+        context.drawImage(this.videoPlayer, 0, 0, 680, 360);
+        this.canvas.toBlob(sendFile);
+        this.videoPlayer.style.display = 'none'
+        console.log(this.videoPlayer)
+        this.state.stream.getVideoTracks().forEach(function(track) {
+            track.stop();
+            track.enabled = false; 
+          });  
+    };
+
     render() {
         return (
-            <div className = "camera-container">
-                <video id = "player"  style = {styles} ref={this.myRef} autoPlay></video>
-                <button className = "capture-btn"><span></span></button>
+            <div className="c-camera-feed">
+                <div className="c-camera-feed__viewer">
+                    <video ref={ref => (this.videoPlayer = ref)} width="680" heigh="360" autoPlay/>
+                </div>
+                <button onClick={this.takePhoto}>Take photo!</button>
+                <div className="c-camera-feed__stage">
+                    <canvas width="680" height="360" ref={ref => (this.canvas = ref)} />
+                </div>
             </div>
-        )
+        );
     }
 }
